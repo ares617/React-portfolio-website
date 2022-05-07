4cd0649c03bdfaeca5834c598eb560a1491144d8
ares617 <danielillolisto@gmail.com>
2022-05-07 15:57:19 +0200

    circulo Home

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Nav from './components/nav/Nav';
 const App = () => {
   return (
       <>
+        <div className='ripple-background'>
+          <div className='circle xlarge shade2'></div>
+          <div className='circle large shade3'></div>
+          <div className='circle medium shade4'></div>
+          <div className='circle small shade5'></div>
+        </div>
         <Home />
         <Nav />
       </>
